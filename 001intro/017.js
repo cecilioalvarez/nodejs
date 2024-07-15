ca1a6650b845fa88ab3c1bf5f7f1ce349f0699a6
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-15 11:57:12 +0200

    promesas

--- 001intro/017.js
+++ 001intro/017.js
@@ -2,9 +2,27 @@
 // son variables que como su nombre indica tendran un valor en el futuro
 
 const promesa= new Promise((resolve, reject) => {
-    resolve('Promesa resuelta');
+  
+    setTimeout(() => {
+        resolve('resuelta1');
+    }, 2000);
 });
 
-promesa.then((resolucion) => {
+
+const promesa2= new Promise((resolve, reject) => {
+  
+    setTimeout(() => {
+        resolve('resuelta2');
+    }, 5000);
+});
+// es el metodo que se ejecutara cuando pasen dos segundos
+/*promesa.then((resolucion) => {
+    console.log(resolucion);
+});*/
+
+Promise.all([promesa, promesa2]).then((resolucion) => {
+  
+
     console.log(resolucion);
 });
+
