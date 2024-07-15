8f7a93e5d0eb128af1fbd501f1fba00b34727edb
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-15 10:40:21 +0200

    nuevos

---
+++ 001intro/008.js
@@ -0,0 +1,8 @@
+// esto es un array de objetos
+//hace falta tener una clase no hace falta
+let objeto1= {nombre:"Juan",edad:25};
+//console.log(objeto1.nombre);
+//console.log(objeto1.edad);
+let listaPersonas = [{ nombre: "Juan", edad: 25 }, { nombre: "Ana", edad: 24 }, { nombre: "Pedro", edad: 30 }];
+//transformacion
+listaPersonas.map((e) => 70-e.edad).forEach((elemento) => console.log(elemento));
