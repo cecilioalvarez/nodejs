6f2f93fa1107ba6f8dfbb2d841e36b3510b8aa81
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-15 09:59:50 +0200

    bucle

---
+++ 001intro/003.js
@@ -0,0 +1,12 @@
+console.log("hola mundo");
+let lista= [1,2,3,4];
+console.log(lista);
+for (let i=0; i<lista.length; i++){
+    console.log(lista[i]);
+}
+let total=0;
+for (let i=0; i<lista.length; i++){
+   total+=lista[i];
+}
+alert("hola");
+console.log(total);
