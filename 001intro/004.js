1f7dc8b0c6badf6e4cf0c47068b95f6d531a80e9
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-15 10:24:15 +0200

    nuevos

--- 001intro/004.js
+++ 001intro/004.js
@@ -11,11 +11,33 @@ for (let i=0; i<lista.length; i++){
 
 //high order function
 
-
-var mifuncion= function(datos){
+//una variable de tipo funcion que nos permite flexibilidad
+var mifuncion1= function(datos){
     console.log(datos);
 }
 
+var mifuncion2= function(datos){
+    console.log("****"+datos+"****");
+}
+//template string de javascript es6
+var mifuncion3= function(datos){
+    console.log(`----${datos}----`);
+}
 
-lista.forEach( mifuncion);
+// esta es una funcion
+// que recibe dos parametros
+// el primer parametro es una lista de elementos o array
+// el segundo parametro es una funcion y ese no esta tan claro
+//lo que habitualmente se llama una high order function
+// es una funcion que recibe como parametro otra funcion
+function formatearLista(lista,funcion){
+    for (let i=0; i<lista.length; i++){
+        funcion(lista[i]);
+    }
+}
+// la plasticidad aque aporta la programacion funcional
+
+formatearLista(lista,mifuncion1);
+formatearLista(lista,mifuncion2);
+formatearLista(lista,mifuncion3);
 
