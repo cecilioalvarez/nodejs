f6bfae786f95711b9a5199e645e039294bf3a317
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-16 10:28:25 +0200

    nuevos

--- 004sqlite/004.js
+++ 004sqlite/004.js
@@ -1,5 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const {PersonaRepository}= require('./repositorios/personaRepository.js');
+const {Persona} = require ("./negocio/persona.js")
 let personaRepository= new PersonaRepository('misql.db');
-
+let persona= new Persona("anita",70);
+personaRepository.insertar(persona);
 
--- 004sqlite/repositorios/personaRepository.js
+++ 004sqlite/repositorios/personaRepository.js
@@ -1,8 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
+
+
 class PersonaRepository {
 
 
-    constructo(dbPath) {
+    constructor(dbPath) {
         this.db= new sqlite3.Database(dbPath, (err) => {
             if (err) {
                 console.error('Error al abrir la base de datos', err.message);
@@ -10,4 +12,15 @@ class PersonaRepository {
             console.log('Conectado a la base de datos');
         });
     }
+
+    insertar(persona) {
+
+        this.db.run('INSERT INTO personas(nombre, edad) VALUES(?,?)', [persona.nombre, persona.edad], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    }
 }
+module.exports = {PersonaRepository};
