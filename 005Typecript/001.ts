6cdee1ed6fdd2de6ec508e7a943d0c4fa802c08b
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-17 09:41:23 +0200

    peticiones post

--- 005Typecript/001.ts
+++ 005Typecript/001.ts
@@ -1,31 +1,46 @@
 import express, { Request, Response } from "express";
+import { Persona } from "./models/Persona";
+import { BodyParser } from "body-parser";
+
+
 import dotenv from "dotenv";
 
 // configures dotenv to work in your application
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
-app.get("/", (request: Request, response: Response) => { 
+const personas = [new Persona("Juan", 30), new Persona("Maria", 25), new Persona("Carlos", 40)];
+
+
+app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
-}); 
+});
 
 app.get("/personas", (request: Request, response: Response) => {
 
 
-  const personas = [
-    { id: 1, nombre: "Juan", apellido: "Perez" },
-    { id: 2, nombre: "Maria", apellido: "Lopez" },
-    { id: 3, nombre: "Carlos", apellido: "Gonzalez" }
-  ];
-
+  
   response.status(200).json(personas);
 
-}) 
+})
 
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
+app.post("/personas", (request: Request, response: Response) => {
+
+
+let persona:Persona= request.body;
+
+personas.push(persona);
+response.status(201).json(persona);
+
+
+})
+
+app.listen(PORT, () => {
+  console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
   // gracefully handle error
   throw new Error(error.message);
---
+++ 005Typecript/models/Persona.ts
@@ -0,0 +1,5 @@
+export class Persona {
+
+    constructor (public nombre: string, public edad: number) {
+    }
+}
