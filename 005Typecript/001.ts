0d7487828eefa9c0a51d463e391d3ee56376d00c
cecilioalvarez <cmac@arquitecturajava.com>
2024-07-17 11:04:48 +0200

    nuevos

--- 005Typecript/001.ts
+++ 005Typecript/001.ts
@@ -17,57 +17,13 @@ const PORT = 3000;
 let personaRepository: PersonaRepository = new PersonaRepository();
 let controlador:PersonaController = new PersonaController(personaRepository);
 
-
 app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
 });
-
 app.get("/personas", controlador.buscarTodos.bind(controlador));
-
-// envias los datos en el body de la peticion y los gestionas
-app.post("/personas", (request: Request, response: Response) => {
-
-
-  let persona: Persona = request.body;
-
-  personaRepository.insertar(persona);
-  response.status(201).json(persona);
-
-
-})
-
-app.get("/personas/:nombre", (request: Request, response: Response) => {
-
-  let nombre: string = request.params.nombre;
-
-  let persona:Persona = personaRepository.buscarUno(nombre);
-
-  if (persona) {
-    response.status(200).json(persona);
-  } else {
-    response.status(404).send("Persona no encontrada");
-  }
-
-})
-
-app.delete("/personas/:nombre", (request: Request, response: Response) => { 
-
-
-  let nombre: string = request.params.nombre;
-  try {
-    
-    let persona= personaRepository.buscarUno(nombre);
-    
-    personaRepository.borrar(persona);
-
-
-    response.status(204).send();
-  } catch (error : any) {
-    response.status(404).send(error.message);
-  }
-  
-})
-
+app.post("/personas", controlador.insertar.bind(controlador));
+app.get("/personas/:nombre", controlador.buscarUno.bind(controlador));
+app.delete("/personas/:nombre", controlador.borrar.bind(controlador));  
 
 app.listen(PORT, () => {
   console.log("Server running at PORT: ", PORT);
--- 005Typecript/controllers/PersonaController.ts
+++ 005Typecript/controllers/PersonaController.ts
@@ -1,12 +1,41 @@
 
 import express, { Request, Response } from "express";
 import { PersonaRepository } from "../repositories/PersonaRepository";
+import { Persona } from "../models/Persona";
 
 export class PersonaController {
     constructor(public personaRepository: PersonaRepository) {
     }
-    
+
     public buscarTodos(request: Request, response: Response): void {
         response.status(200).json(this.personaRepository.buscarTodos());
+    }
+
+    public insertar(request: Request, response: Response): void {
+
+        let persona: Persona = request.body;
+        this.personaRepository.insertar(persona);
+        response.status(201).json(persona);
+
+    }
+    public borrar(request: Request, response: Response): void {
+
+        let nombre: string = request.params.nombre;
+        let persona = this.personaRepository.buscarUno(nombre);
+        this.personaRepository.borrar(persona);
+        response.status(204).send();
+
+    }
+
+    public buscarUno(request: Request, response: Response): void {
+        let nombre: string = request.params.nombre;
+
+        let persona:Persona = this.personaRepository.buscarUno(nombre);
+      
+        if (persona) {
+          response.status(200).json(persona);
+        } else {
+          response.status(404).send("Persona no encontrada");
+        }
     }
 }
